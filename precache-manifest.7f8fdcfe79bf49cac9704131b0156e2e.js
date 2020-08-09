a6adee17d6e5a7e8c456826cc694201da37834b4
Mayank <mayankipsa@gmail.com>
2020-08-09 21:13:04 +0530

    Updates

---
+++ precache-manifest.7f8fdcfe79bf49cac9704131b0156e2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3592c82363e1d34fac7109e04f08c337",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "f8968f19b580c6bb6c7c",
+    "url": "/tictactoe/static/css/main.38f2be2c.chunk.css"
+  },
+  {
+    "revision": "a740ed8f3265a1da9ecc",
+    "url": "/tictactoe/static/js/2.af71590a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tictactoe/static/js/2.af71590a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8968f19b580c6bb6c7c",
+    "url": "/tictactoe/static/js/main.9dc96587.chunk.js"
+  },
+  {
+    "revision": "0296dfef4fc7a36c4d4a",
+    "url": "/tictactoe/static/js/runtime-main.f028c8b1.js"
+  }
+]);
